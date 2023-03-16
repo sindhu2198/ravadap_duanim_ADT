cf2686db0fba227cf93b15afced53c0991942f23
Durga Sindhu Animalla <duanim@iu.edu>
2023-03-16 00:06:18 -0400

    modied nav

--- client/src/Hamburgermenu.js
+++ client/src/Hamburgermenu.js
@@ -1,15 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./Hamburgermenu.css";
 
-const HamburgerMenu = () => {
+const Hamburgermenu = () => {
   const [showMenu, setShowMenu] = useState(false);
+  const menuRef = useRef();
 
   const handleClick = () => {
     setShowMenu(!showMenu);
   };
 
+  const handleClickOutside = (event) => {
+    if (menuRef.current && !menuRef.current.contains(event.target)) {
+      setShowMenu(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
   return (
-    <div className="hamburger-menu-container">
+    <div className="hamburger-menu-container" ref={menuRef}>
       <div className="hamburger-icon" onClick={handleClick}>
         <div className="icon-line"></div>
         <div className="icon-line"></div>
@@ -28,4 +41,4 @@ const HamburgerMenu = () => {
   );
 };
 
-export default HamburgerMenu;
+export default Hamburgermenu;
